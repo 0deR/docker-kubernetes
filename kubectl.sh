d88e310d65c248cf46f360e246d9648a700a2135
Rido Akbar Ibrahim <57303169+0deR@users.noreply.github.com>
2021-07-22 20:35:57 +0700

    Update kubectl.sh

--- kubectl.sh
+++ kubectl.sh
@@ -6,7 +6,8 @@ sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg  lsb-release
+    lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg  
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
