4818711bf9c611b43db98b7aca4a9a4db19bf4f2
Rido Akbar Ibrahim <57303169+0deR@users.noreply.github.com>
2021-07-25 17:55:34 +0700

    Update kops_create.sh

--- kops_create.sh
+++ kops_create.sh
@@ -5,14 +5,14 @@ kops create cluster \
    --networking="weave" \
    --master-zones="us-east-2a,us-east-2b,us-east-2c" \
    --zones="us-east-2a,us-east-2b,us-east-2c" \
-   --master-size="t2.medium" \
-   --master-count="3" \
-   --node-size="t2.medium" \
-   --node-count="3" \
+   --master-size="t2.micro" \
+   --master-count="1" \
+   --node-size="t2.micro" \
+   --node-count="1" \
    --state="s3://ibrahimsite.xyz" \
    --topology="private" \
-   --subnets="subnet-0ced12342,subnet-036dw235fwr32,subnet-0w54fgrgt45232" #private-subnet\
+   --subnets="subnet-082a8129dc6644322,subnet-05737ec51001f971c,subnet-07d4ba5050d4cb788" #private-subnet\
    --bastion="true" \
-   --utility-subnets="subnet-09infdsjtr2fdf5fe,subnet-43sf54dfsafwe43rg,subnet-04ft4vfdgbthrhgv"#public subnet for bastion \
-   --vpc="vpc-03rrtgvtr5525tfvdfv" #vpcid\
+   --utility-subnets="subnet-0834c4e14f53c1ef7,subnet-01e549467e10b858d,subnet-0b1f7a0863d06af82"#public subnet for bastion \
+   --vpc="vpc-034ce103af94656f1" #vpcid\
    --dry-run -o yaml > cluster.yaml
